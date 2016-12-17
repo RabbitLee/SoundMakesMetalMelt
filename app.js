6707e6f4deb36f2028ab8f03837ed9091315b3ab
RabbitLee <yclissetj@gmail.com>
2016-12-17 16:14:22 +0800

    get melt time from web a give it to arduino

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var get_config = require('./routes/get_config');
+var media_info = require('./routes/media_info');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use(express.static('views'));
 // app.use('/', index);
 app.use('/', users);
 app.use('/get_config', get_config);
+app.use('/media_info', media_info);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/media_info.js
@@ -0,0 +1,20 @@
+/**
+ * Created by RabbitLee on 16/12/2016.
+ */
+
+var express = require('express');
+var router = express.Router();
+
+var voice_time = 0;
+
+router.get('/write', function(req, res, next) {
+    voice_time = req.query.voice_time;
+    res.sendStatus(200);
+});
+
+router.get('/read', function (req, res, next) {
+    res.send({'heat_time': voice_time});
+    voice_time = 0;
+})
+
+module.exports = router;
